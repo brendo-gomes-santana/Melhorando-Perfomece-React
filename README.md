264b9fb5cfe84cab781ac9712f1ed76befac8c83
Brendo Gomes Santana <brendogs2021@gmail.com>
2023-08-10 14:34:17 -0400

    utilizando o ZOD para fazer validação junto com react-hook-form

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 parando de usar useState e começa a usar useRef.
 
 biblioteca react-hook-form -> npm i react-hook-form
+biblioteca ZOD -> npm i zod
+
+não é necessário instalar o resolvers
+resolver é o formado que vai ser integrado com react-hook-form
+
+npm i @hookform/resolvers
--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,56 @@
-import { useRef } from 'react'
-import './App.css'
-import { Header } from './Header';
-import { useForm } from 'react-hook-form'
+import "./App.css";
+import { Header } from "./Header";
+import { useForm } from "react-hook-form";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  name: z.string().nonempty("O campo é obrigatório"),
+  email: z
+    .string()
+    .email("Digite um email valido.")
+    .nonempty("O campos é obrigatório"),
+  username: z
+    .string()
+    .min(3, "o username dever ter pelo menos 3 caracteres")
+    .max(10, "o username dever no maximo 3 caracteres")
+    .nonempty("o campos é obrigatório"),
+});
+
+//nonempty é obrigatório
+//min quando eu coloquei o numero e depois mensagem, e tipo o min e caso não tenha isso, a mensagem de error
 
 function App() {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: zodResolver(schema),
+  });
 
-  const { register, handleSubmit } = useForm()
- 
-  function handleSave(data){
-   
-    console.log(data) // tudo junto
-    console.log(data.nome) // ou pegar algo
+  function handleSave(data) {
+    console.log(data); // tudo junto
+    console.log(data.nome); // ou pegar algo
   }
-  
- 
 
   return (
     <div className="container">
       <h1>React</h1>
-      <Header/>
+      <Header />
 
       <form className="form" onSubmit={handleSubmit(handleSave)}>
         <input
           type="text"
-          placeholder="Digite seu nome..."
-          {...register("name", { required: true })} // é obrigatório preencher
+          style={errors.name && { border: '1px solid red'}}
+          placeholder={ errors.name ? errors.name.message : 'Digite seu nome' }
+          {...register("name", { required: true })} // é obrigatório preencher - tipo de validação pelo react-hook-form
           className="input"
         />
-
         <input
           type="text"
           placeholder="Digite seu email..."
-          {...register("email", {required: true})}
+          {...register("email")}
           className="input"
         />
-
+        {errors.email && <span>{errors.email.message}</span> /*outra formar de fazer*/}
         <input
           type="text"
           placeholder="Digite seu username..."
@@ -42,11 +58,12 @@ function App() {
           className="input"
         />
 
-
-        <button className="button" type="submit">Enviar</button>
+        <button className="button" type="submit">
+          Enviar
+        </button>
       </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
